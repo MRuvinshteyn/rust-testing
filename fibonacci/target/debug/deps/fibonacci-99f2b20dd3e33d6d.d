830b5ec9e6fecdb7b1b51ae8a364d8e569a15e97
Michael Ruvinshteyn <michael.ruvinshteyn15@myhunter.cuny.edu>
2019-02-21 22:22:18 -0500

    added windows support for fibonacci

---
+++ fibonacci/target/debug/deps/fibonacci-99f2b20dd3e33d6d.d
@@ -0,0 +1,5 @@
+C:\Users\micha\OneDrive\Documents\rust\git\rust-testing\fibonacci\target\debug\deps\fibonacci-99f2b20dd3e33d6d.exe: src\main.rs
+
+C:\Users\micha\OneDrive\Documents\rust\git\rust-testing\fibonacci\target\debug\deps\fibonacci-99f2b20dd3e33d6d.d: src\main.rs
+
+src\main.rs:
--- fibonacci/target/debug/fibonacci.d
+++ fibonacci/target/debug/fibonacci.d
@@ -1,1 +1,1 @@
-/home/michael/Documents/rust/git/rust-testing/fibonacci/target/debug/fibonacci: /home/michael/Documents/rust/git/rust-testing/fibonacci/src/main.rs
+C:\Users\micha\OneDrive\Documents\rust\git\rust-testing\fibonacci\target\debug\fibonacci.pdb: C:\Users\micha\OneDrive\Documents\rust\git\rust-testing\fibonacci\src\main.rs
