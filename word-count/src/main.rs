282069aedfef0dd25359645fefdb8a863771131d
Michael Ruvinshteyn <michael.ruvinshteyn15@myhunter.cuny.edu>
2019-03-04 21:47:25 -0500

    added basic word count program

---
+++ word-count/src/main.rs
@@ -0,0 +1,32 @@
+#![allow(dead_code)]
+
+use std::error::Error;
+use std::fs::File;
+use std::path::Path;
+use std::io::Read;
+
+fn word_count(s:String) -> i64 {
+    let mut cnt:i64 = 0i64;
+    for _i in s.split_whitespace() {
+        cnt += 1;
+    }
+    cnt
+}
+
+fn main() -> std::io::Result<()> {
+//    let path = Path::new("words.txt");
+    let curr = std::env::current_dir()?;
+    let path = curr.join(Path::new("target/debug/words.txt"));
+    let mut file = match File::open(&path) {
+        Ok(file) => file,
+        Err(err) => panic!("{}", err.description())
+    };
+    let mut s:String = String::new();
+    match file.read_to_string(&mut s) {
+        Ok(_) => println!("{}", word_count(s)),
+        Err(err) => panic!("{}", err.description())
+    }
+    println!("{}", curr.display());
+    Ok(())
+    
+}
---
+++ word-count/target/debug/word-count.d
@@ -0,0 +1,1 @@
+/home/michael/Documents/rust/git/rust-testing/word-count/target/debug/word-count: /home/michael/Documents/rust/git/rust-testing/word-count/src/main.rs
