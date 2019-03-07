f246530271b56463ca43b3eef2b88e9eb47c63fd
Michael Ruvinshteyn <michael.ruvinshteyn15@myhunter.cuny.edu>
2019-03-07 14:44:03 -0500

    modified insertion sort code

--- sort/insertion/src/main.rs
+++ sort/insertion/src/main.rs
@@ -1,12 +1,14 @@
 fn insertion_sort(v:&mut Vec<i64>) -> () {
-    for i in 1..v.len() {
-        let key:i64 = v[i as usize];
-        let mut j:i64 = i as i64 - 1;
-        while j >= 0 && v[j as usize] > key {
-            v[(j + 1) as usize] = v[j as usize];
-            j -= 1;
+    let mut j:usize;
+    let mut temp:i64;
+    for i in (0..(v.len() - 1)).rev() {
+        j = i;
+        while j < v.len() - 1 && v[j] > v[j + 1] {
+            temp = v[j];
+            v[j] = v[j + 1];
+            v[j + 1] = temp;
+            j += 1;
         }
-        v[(j + 1) as usize] = key;
     }
 }
 
